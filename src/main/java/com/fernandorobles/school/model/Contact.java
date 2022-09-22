db4787b3d2f602fdd64d004bd5668ce5fe541cc7
fernando robles <advancexz@gmail.com>
2022-09-22 11:26:56 -0700

    contact.html updated with front-end validation on name and email fields; pom.xml updated with spring-boot-starter-validation dependency; Contact.java updated with back-end validation for each field, NotBlank, Size, Pattern and email;

--- src/main/java/com/fernandorobles/school/model/Contact.java
+++ src/main/java/com/fernandorobles/school/model/Contact.java
@@ -2,13 +2,32 @@ package com.fernandorobles.school.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 public class Contact {
 
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 3, message = "Name must be at least 3 characters long")
     private String name;
+
+    @NotBlank(message = "Mobile number must not be blank")
+    @Pattern(regexp = "(^$|\\d{10})", message = "Mobile number must be 10 digit number")
     private String mobileNum;
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Please provide a valid email")
     private String email;
+
+    @NotBlank(message = "Subject must not be blank")
+    @Size(min = 5, message = "Subject must be at least 5 characters long")
     private String subject;
+
+    @NotBlank(message = "Message must not be blank")
+    @Size(min = 10, message = "Message must be at least 10 characters long")
     private String message;
 
 }
