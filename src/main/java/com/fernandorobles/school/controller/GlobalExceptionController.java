90b7f58dff24147bd865900eb296dc2e4b3f622c
fernando robles <advancexz@gmail.com>
2022-09-28 10:22:55 -0700

    dribbble.gif added to show error; GlobalExceptionController.java added to handle exceptions across app. error.html view added to show error message to unauthorized navigation login.html updated with message error while login if present. csrf config updated within ProjectSecurityConfig.java, its enabled now and just disabled for /saveMsg  style-starter.css updated with error styles

---
+++ src/main/java/com/fernandorobles/school/controller/GlobalExceptionController.java
@@ -0,0 +1,19 @@
+package com.fernandorobles.school.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionController {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView exceptionHandles(Exception e){
+        ModelAndView errorPage = new ModelAndView();
+        errorPage.setViewName("error");
+        errorPage.addObject("errormsg", e.getMessage());
+        return errorPage;
+    }
+}
