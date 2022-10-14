def6fc0f58e006d7d64bd529608e94bde837c84b
fernando robles <advancexz@gmail.com>
2022-10-14 11:08:24 -0700

    DashboardController.java updated with a demo log method to show error, warn, info, debug and trace messages;  application.properties updated with properties to define a log group and a property to print messages colored;  logback.xml added to output the log, log archive will be backed up after reach 10MB;

--- src/main/java/com/fernandorobles/school/controller/DashboardController.java
+++ src/main/java/com/fernandorobles/school/controller/DashboardController.java
@@ -27,6 +27,16 @@ public class DashboardController {
             model.addAttribute("enrolledClass", person.getSchoolClass().getName());
         }
         session.setAttribute("loggedInPerson", person);
+        logMessages();
         return "dashboard.html";
+    }
+
+    private void logMessages(){
+        log.error("error message from dashboard");
+        log.warn("warn message from dashboard");
+        log.info("info message from dashboard");
+        log.debug("debug message from dashboard");
+        log.trace("trace message from dashboard");
+
     }
 }
